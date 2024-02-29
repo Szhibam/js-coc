ffd8178e2360d34c8eed180f46f98c06fbda8b95
Szhibam <shibambanerjeezxofficial@gmail.com>
2024-02-29 18:28:24 +0530

    scope in functions-->global and block

---
+++ 03_basics/02_scopes.js
@@ -0,0 +1,20 @@
+ var c = 100
+
+ if(true){          //blockspace
+    let a = 10      //blockspace
+    const b = 20    //blockspace
+    var c = 30      //blockspace
+ }
+
+ /*
+ >> console.log(a);
+ :: a is not defined
+ 
+ >> console.log(b);
+ :: b is not defined
+
+ >> console.log(c);
+ :: 30
+ */
+
+ //SCOPE ASSESSED THROUGH BROWSER AND THROUGH CODE ENVIROMNEBT(USING NODE.JS) ARE DIFFERENT
